74b59f3dc2eee7b1b4364b5ba665fb70332a911b
Chris Loper <chrisloper@utexas.edu>
2022-04-27 11:16:33 -0500

    all user_api_test

--- tests/user_api_test.rb
+++ tests/user_api_test.rb
@@ -11,4 +11,29 @@ class TestUserAPI < Test::Unit::TestCase
         user = PassageClient.user.get_user(user_id: ENV["USER_ID"])
         assert_equal ENV["USER_ID"], user.id 
     end
+    
+    def test_deactivate_user()
+        user = PassageClient.user.deactivate_user(user_id: ENV["USER_ID"])
+        assert_equal ENV["USER_ID"], user.id
+        assert_equal "inactive", user.status
+    end
+
+    def test_activate_user()
+        user = PassageClient.user.activate_user(user_id: ENV["USER_ID"])
+        assert_equal ENV["USER_ID"], user.id
+        assert_equal "active", user.status
+    end
+
+    def test_update_user()
+        user = PassageClient.user.get_user(user_id: ENV["USER_ID"])
+        original_email = user.email
+        new_email = "chris+update_test@passage.id"
+        user = PassageClient.user.update_user(user_id: ENV["USER_ID"], email: new_email)
+        assert_equal ENV["USER_ID"], user.id
+        assert_equal new_email, user.email
+        user = PassageClient.user.update_user(user_id: ENV["USER_ID"], email: original_email)
+        assert_equal ENV["USER_ID"], user.id
+        assert_equal original_email, user.email
+    end
+
 end
