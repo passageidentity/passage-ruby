13e63aa2b90cd73cfa912e6b0ba31e78cb488a04
Chris Loper <chrisloper@utexas.edu>
2022-05-19 12:37:03 -0500

    get_app addition

---
+++ tests/app_test.rb
@@ -0,0 +1,15 @@
+require_relative '../lib/passageidentity/client'
+require 'dotenv'
+require 'faraday'
+require 'test/unit'
+
+Dotenv.load('.env')
+class TestAppAPI < Test::Unit::TestCase
+  PassageClient =
+    Passage::Client.new(app_id: ENV['APP_ID'], api_key: ENV['API_KEY'])
+
+  def test_get_app() 
+    app = PassageClient.get_app()
+    assert_equal ENV['APP_ID'], app.id
+  end
+end
