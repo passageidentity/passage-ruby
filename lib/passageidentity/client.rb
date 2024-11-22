272c6fa71d56021b41e0652a80326a8dd0635b7b
Vanessa Burroughs <vanessa.burroughs@agilebits.com>
2024-11-22 03:24:38 +0000

    refactor: remove create_magic_link warn

--- lib/passageidentity/client.rb
+++ lib/passageidentity/client.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
+require 'rubygems'
 require_relative 'auth'
 require_relative 'user_api'
 require_relative 'error'
 require_relative 'version'
-require 'rubygems'
 require_relative '../openapi_client'
 
 module Passage
@@ -17,6 +17,8 @@ module Passage
   # The Passage::Client class provides methods for interacting with Passage
   class Client
     attr_reader :auth, :user
+
+    extend Gem::Deprecate
 
     def initialize(app_id:, api_key: '', auth_strategy: COOKIE_STRATEGY)
       @app_id = app_id
@@ -45,7 +47,7 @@ module Passage
     end
     # rubocop:enable Naming/AccessorMethodName
 
-    # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/ParameterLists, Metrics/AbcSize
+    # rubocop:disable Metrics/ParameterLists
     def create_magic_link(
       user_id: '',
       email: '',
@@ -58,52 +60,21 @@ module Passage
       ttl: 60,
       type: 'login'
     )
-      warn '[DEPRECATION] `create_magic_link()` is deprecated.  Please use `auth.create_magic_link()` instead.'
-
-      magic_link_req = {}
-      magic_link_req['user_id'] = user_id unless user_id.empty?
-      magic_link_req['email'] = email unless email.empty?
-      magic_link_req['phone'] = phone unless phone.empty?
-
-      # check to see if the channel specified is valid before sending it off to the server
-      unless [PHONE_CHANNEL, EMAIL_CHANNEL].include? channel
-        raise PassageError.new(
-          message:
-            'channel: must be either Passage::EMAIL_CHANNEL or Passage::PHONE_CHANNEL'
-        )
-      end
-      magic_link_req['channel'] = channel unless channel.empty?
-      magic_link_req['send'] = send
-      unless magic_link_path.empty?
-        magic_link_req[
-          'magic_link_path'
-        ] = magic_link_path
-      end
-      magic_link_req['redirect_url'] = redirect_url unless redirect_url.empty?
-      magic_link_req['language'] = language
-      magic_link_req['ttl'] = ttl unless ttl.zero?
-      magic_link_req['type'] = type
-
-      begin
-        gemspec = File.join(__dir__, '../../passageidentity.gemspec')
-        Gem::Specification.load(gemspec)
-        header_params = { 'Passage-Version' => "passage-ruby #{Passage::VERSION}" }
-        header_params['Authorization'] = "Bearer #{@api_key}" if @api_key != ''
-
-        opts = {}
-        opts[:header_params] = header_params
-        opts[:debug_auth_names] = ['header']
-
-        client = OpenapiClient::MagicLinksApi.new
-        client.create_magic_link(@app_id, magic_link_req, opts).magic_link
-      rescue Faraday::Error => e
-        raise PassageError.new(
-          message: 'failed to create Passage Magic Link',
-          status_code: e.response[:status],
-          body: e.response[:body]
-        )
-      end
+      @auth.create_magic_link(
+        user_id: user_id,
+        email: email,
+        phone: phone,
+        channel: channel,
+        send: send,
+        magic_link_path: magic_link_path,
+        redirect_url: redirect_url,
+        language: language,
+        ttl: ttl,
+        type: type
+      )
     end
-    # rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/ParameterLists, Metrics/AbcSize
+    # rubocop:enable Metrics/ParameterLists
+
+    deprecate(:create_magic_link, 'Passage::Auth.create_magic_link', 2024, 12)
   end
 end
