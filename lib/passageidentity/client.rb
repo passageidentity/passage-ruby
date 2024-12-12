10c98703a8beda1380fae88bb930a4eb3ed5765b
Chris Tran <ctran88@users.noreply.github.com>
2024-12-12 10:43:55 -0600

    feat: reworks the new create magic link func into three separate functions (#121)

--- lib/passageidentity/client.rb
+++ lib/passageidentity/client.rb
@@ -37,11 +37,20 @@ module Passage
 
       @auth_strategy = auth_strategy
 
+      header_params = { 'Passage-Version' => "passage-ruby #{Passage::VERSION}" }
+      header_params['Authorization'] = "Bearer #{@api_key}" if @api_key != ''
+
+      @req_opts = {}
+      @req_opts[:header_params] = header_params
+      @req_opts[:debug_auth_names] = ['header']
+
       # initialize auth class
       @auth = Passage::Auth.new(@app_id, @api_key, @auth_strategy)
 
       # initialize user class
       @user = Passage::UserAPI.new(@app_id, @api_key)
+
+      @magic_links_client = OpenapiClient::MagicLinksApi.new
     end
 
     # rubocop:disable Naming/AccessorMethodName
@@ -53,7 +62,7 @@ module Passage
     end
     # rubocop:enable Naming/AccessorMethodName
 
-    # rubocop:disable Metrics/ParameterLists
+    # rubocop:disable Metrics/ParameterLists, Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
     def create_magic_link(
       user_id: '',
       email: '',
@@ -66,20 +75,37 @@ module Passage
       ttl: 60,
       type: 'login'
     )
-      @auth.create_magic_link(
-        user_id: user_id,
-        email: email,
-        phone: phone,
-        channel: channel,
-        send: send,
-        magic_link_path: magic_link_path,
-        redirect_url: redirect_url,
-        language: language,
-        ttl: ttl,
-        type: type
-      )
+      args = {}
+      args['user_id'] = user_id unless user_id.empty?
+      args['email'] = email unless email.empty?
+      args['phone'] = phone unless phone.empty?
+
+      unless [PHONE_CHANNEL, EMAIL_CHANNEL].include? channel
+        raise PassageError.new(
+          message:
+            'channel: must be either Passage::EMAIL_CHANNEL or Passage::PHONE_CHANNEL'
+        )
+      end
+
+      args['channel'] = channel unless channel.empty?
+      args['type'] = type
+      args['send'] = send
+
+      args['language'] = language unless language.empty?
+      args['magic_link_path'] = magic_link_path unless magic_link_path.empty?
+      args['redirect_url'] = redirect_url unless redirect_url.empty?
+      args['ttl'] = ttl unless ttl.zero?
+
+      begin
+        @magic_links_client.create_magic_link(@app_id, args, @req_opts).magic_link
+      rescue Faraday::Error => e
+        raise PassageError.new(
+          status_code: e.response[:status],
+          body: e.response[:body]
+        )
+      end
     end
-    # rubocop:enable Metrics/ParameterLists
+    # rubocop:enable Metrics/ParameterLists, Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 
     deprecate(:create_magic_link, 'Passage::Auth.create_magic_link', 2025, 1)
     deprecate(:get_app, :none, 2025, 1)
