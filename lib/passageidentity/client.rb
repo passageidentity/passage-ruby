ea7d56eb708ac7bfb3ee3ecd88b62e883a979fd1
Chris Tran <chris.tran@agilebits.com>
2024-12-23 16:20:47 -0600

    feat!: update client ctor signature

--- lib/passageidentity/client.rb
+++ lib/passageidentity/client.rb
@@ -2,7 +2,6 @@
 
 require_relative 'auth'
 require_relative 'user_api'
-require_relative 'error'
 require_relative 'version'
 
 module Passage
@@ -16,32 +15,25 @@ module Passage
   class Client
     attr_reader :auth, :user
 
-    def initialize(app_id:, api_key: '', auth_strategy: COOKIE_STRATEGY)
-      @app_id = app_id
-      @api_key = api_key
-
-      # check for valid auth strategy
-      unless [COOKIE_STRATEGY, HEADER_STRATEGY].include? auth_strategy
-        raise PassageError.new(
-          status_code: 400,
-          body: {
-            error: 'Invalid auth strategy',
-            code: 'invalid_argument'
-          }
-        )
+    def initialize(app_id:, api_key:)
+      unless app_id && !app_id.empty?
+        raise ArgumentError,
+              'A Passage App ID is required. Please include (app_id: YOUR_APP_ID, api_key: YOUR_API_KEY).'
+      end
+      unless api_key && !api_key.empty?
+        raise ArgumentError,
+              'A Passage API key is required. Please include (app_id: YOUR_APP_ID, api_key: YOUR_API_KEY).'
       end
 
-      @auth_strategy = auth_strategy
+      req_opts = {}
+      req_opts[:header_params] = {
+        'Passage-Version' => "passage-ruby #{Passage::VERSION}",
+        'Authorization' => "Bearer #{api_key}"
+      }
+      req_opts[:debug_auth_names] = ['header']
 
-      header_params = { 'Passage-Version' => "passage-ruby #{Passage::VERSION}" }
-      header_params['Authorization'] = "Bearer #{@api_key}" if @api_key != ''
-
-      @req_opts = {}
-      @req_opts[:header_params] = header_params
-      @req_opts[:debug_auth_names] = ['header']
-
-      @auth = Passage::Auth.new(@app_id, @api_key, @auth_strategy)
-      @user = Passage::UserAPI.new(@app_id, @api_key)
+      @auth = Passage::Auth.new(app_id: app_id, req_opts: req_opts)
+      @user = Passage::UserAPI.new(app_id: app_id, req_opts: req_opts)
     end
   end
 end
