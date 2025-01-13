827bc5ceefab5270d76073879ead423fce88bb8b
Chris Tran <ctran88@users.noreply.github.com>
2025-01-13 11:26:54 -0600

    fix: adds error handling for OpenApi errors (#166)

--- lib/passageidentity/user.rb
+++ lib/passageidentity/user.rb
@@ -15,11 +15,16 @@ module Passage
     end
 
     def get(user_id:)
-      raise ArgumentError, 'user_id is required.' unless user_id && !user_id.empty?
+      raise ArgumentError, 'user_id is required.' if blank_str?(user_id)
 
       begin
         response = @user_client.get_user(@app_id, user_id, @req_opts)
         response.user
+      rescue OpenapiClient::ApiError => e
+        raise PassageError.new(
+          status_code: e.code,
+          body: try_parse_json_string(e.response_body)
+        )
       rescue Faraday::Error => e
         raise PassageError.new(
           status_code: e.response[:status],
@@ -29,11 +34,16 @@ module Passage
     end
 
     def get_by_identifier(identifier:)
-      raise ArgumentError, 'identifier is required.' unless identifier && !identifier.empty?
+      raise ArgumentError, 'identifier is required.' if blank_str?(identifier)
 
       begin
         req_opts = set_get_by_identifier_query_params(identifier: identifier)
         response = @user_client.list_paginated_users(@app_id, req_opts)
+      rescue OpenapiClient::ApiError => e
+        raise PassageError.new(
+          status_code: e.code,
+          body: try_parse_json_string(e.response_body)
+        )
       rescue Faraday::Error => e
         raise PassageError.new(
           status_code: e.response[:status],
@@ -45,11 +55,16 @@ module Passage
     end
 
     def activate(user_id:)
-      raise ArgumentError, 'user_id is required.' unless user_id && !user_id.empty?
+      raise ArgumentError, 'user_id is required.' if blank_str?(user_id)
 
       begin
         response = @user_client.activate_user(@app_id, user_id, @req_opts)
         response.user
+      rescue OpenapiClient::ApiError => e
+        raise PassageError.new(
+          status_code: e.code,
+          body: try_parse_json_string(e.response_body)
+        )
       rescue Faraday::Error => e
         raise PassageError.new(
           status_code: e.response[:status],
@@ -59,11 +74,16 @@ module Passage
     end
 
     def deactivate(user_id:)
-      raise ArgumentError, 'user_id is required.' unless user_id && !user_id.empty?
+      raise ArgumentError, 'user_id is required.' if blank_str?(user_id)
 
       begin
         response = @user_client.deactivate_user(@app_id, user_id, @req_opts)
         response.user
+      rescue OpenapiClient::ApiError => e
+        raise PassageError.new(
+          status_code: e.code,
+          body: try_parse_json_string(e.response_body)
+        )
       rescue Faraday::Error => e
         raise PassageError.new(
           status_code: e.response[:status],
@@ -73,35 +93,57 @@ module Passage
     end
 
     def update(user_id:, options:)
-      raise ArgumentError, 'user_id is required.' unless user_id && !user_id.empty?
-      raise ArgumentError, 'options are required.' unless options && !options.empty?
+      raise ArgumentError, 'user_id is required.' if blank_str?(user_id)
+      raise ArgumentError, 'options are required.' if options.empty?
 
-      response = @user_client.update_user(@app_id, user_id, options, @req_opts)
-      response.user
-    rescue Faraday::Error => e
-      raise PassageError.new(
-        status_code: e.response[:status],
-        body: e.response[:body]
-      )
+      begin
+        response = @user_client.update_user(@app_id, user_id, options, @req_opts)
+        response.user
+      rescue OpenapiClient::ApiError => e
+        raise PassageError.new(
+          status_code: e.code,
+          body: try_parse_json_string(e.response_body)
+        )
+      rescue Faraday::Error => e
+        raise PassageError.new(
+          status_code: e.response[:status],
+          body: e.response[:body]
+        )
+      end
     end
 
     def create(args:)
-      raise ArgumentError, 'At least one of args.email or args.phone is required.' unless args['phone'] || args['email']
+      if blank_str?(args['email']) && blank_str?(args['phone'])
+        raise ArgumentError,
+              'At least one of args.email or args.phone is required.'
+      end
 
-      response = @user_client.create_user(@app_id, args, @req_opts)
-      response.user
-    rescue Faraday::Error => e
-      raise PassageError.new(
-        status_code: e.response[:status],
-        body: e.response[:body]
-      )
+      begin
+        response = @user_client.create_user(@app_id, args, @req_opts)
+        response.user
+      rescue OpenapiClient::ApiError => e
+        raise PassageError.new(
+          status_code: e.code,
+          body: try_parse_json_string(e.response_body)
+        )
+      rescue Faraday::Error => e
+        raise PassageError.new(
+          status_code: e.response[:status],
+          body: e.response[:body]
+        )
+      end
     end
 
     def delete(user_id:)
-      raise ArgumentError, 'user_id is required.' unless user_id && !user_id.empty?
+      raise ArgumentError, 'user_id is required.' if blank_str?(user_id)
 
       begin
         @user_client.delete_user(@app_id, user_id, @req_opts)
+      rescue OpenapiClient::ApiError => e
+        raise PassageError.new(
+          status_code: e.code,
+          body: try_parse_json_string(e.response_body)
+        )
       rescue Faraday::Error => e
         raise PassageError.new(
           'failed to delete Passage User',
@@ -112,11 +154,16 @@ module Passage
     end
 
     def revoke_device(user_id:, device_id:)
-      raise ArgumentError, 'user_id is required.' unless user_id && !user_id.empty?
-      raise ArgumentError, 'device_id is required.' unless device_id && !device_id.empty?
+      raise ArgumentError, 'user_id is required.' if blank_str?(user_id)
+      raise ArgumentError, 'device_id is required.' if blank_str?(device_id)
 
       begin
         @user_device_client.delete_user_devices(@app_id, user_id, device_id, @req_opts)
+      rescue OpenapiClient::ApiError => e
+        raise PassageError.new(
+          status_code: e.code,
+          body: try_parse_json_string(e.response_body)
+        )
       rescue Faraday::Error => e
         raise PassageError.new(
           status_code: e.response[:status],
@@ -126,11 +173,16 @@ module Passage
     end
 
     def list_devices(user_id:)
-      raise ArgumentError, 'user_id is required.' unless user_id && !user_id.empty?
+      raise ArgumentError, 'user_id is required.' if blank_str?(user_id)
 
       begin
         response = @user_device_client.list_user_devices(@app_id, user_id, @req_opts)
         response.devices
+      rescue OpenapiClient::ApiError => e
+        raise PassageError.new(
+          status_code: e.code,
+          body: try_parse_json_string(e.response_body)
+        )
       rescue Faraday::Error => e
         raise PassageError.new(
           status_code: e.response[:status],
@@ -140,10 +192,15 @@ module Passage
     end
 
     def revoke_refresh_tokens(user_id:)
-      raise ArgumentError, 'user_id is required.' unless user_id && !user_id.empty?
+      raise ArgumentError, 'user_id is required.' if blank_str?(user_id)
 
       begin
         @tokens_client.revoke_user_refresh_tokens(@app_id, user_id, @req_opts)
+      rescue OpenapiClient::ApiError => e
+        raise PassageError.new(
+          status_code: e.code,
+          body: try_parse_json_string(e.response_body)
+        )
       rescue Faraday::Error => e
         raise PassageError.new(
           status_code: e.response[:status],
@@ -153,6 +210,11 @@ module Passage
     end
 
     private
+
+    def blank_str?(str)
+      blank_pattern = /\A[[:space:]]*\z/
+      str.empty? || blank_pattern.match?(str)
+    end
 
     def set_get_by_identifier_query_params(identifier:)
       req_opts = @req_opts.dup
@@ -173,6 +235,12 @@ module Passage
       end
 
       get(user_id: users.first.id)
+    end
+
+    def try_parse_json_string(string)
+      JSON.parse(string)
+    rescue JSON::ParserError
+      string
     end
   end
 end
